be22ff6e993ec2d2793862361fa01b5bc620d9e7
zhuoqian <zhuoqian10@gmail.com>
2020-11-25 14:28:23 +0800

    add more extensions

---
+++ src/api/Session/Extension.Create.cs
@@ -0,0 +1,20 @@
+﻿using Google.Protobuf;
+
+namespace NeoFS.API.v2.Session
+{
+    public partial class CreateResponse : IResponse
+    {
+        public IMessage GetBody()
+        {
+            return Body;
+        }
+    }
+
+    public partial class CreateRequest : IRequest
+    {
+        public IMessage GetBody()
+        {
+            return Body;
+        }
+    }
+}
